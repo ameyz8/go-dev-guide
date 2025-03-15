23279129782b5e1526b24f18c6eb1fbd99c2bd71
ameyz7 <amey9765@hotmail.com>
2025-03-15 16:30:02 +0530

    custom-types-receiver-functions | go approach

--- cards/main.go
+++ cards/main.go
@@ -1,12 +1,12 @@
 package main
 
-import "fmt"
-
 func main() {
-	card := newCard()
-	fmt.Println(card)
+	cards := deck{"Ace of Diamonds", newCard()}
+	cards = append(cards, "Six of Spades")
+
+	cards.print()
 }
 
 func newCard() string {
-	return "Five of diamonds"
+	return "Five of Diamonds"
 }
