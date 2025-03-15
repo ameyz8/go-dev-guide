d9e36f81a2bb9a430a75316b61fe3eec39162d97
ameyz7 <amey9765@hotmail.com>
2025-03-15 19:54:40 +0530

    write-to-drive | create new folder and write a file to it

--- cards/deck.go
+++ cards/deck.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
 // create a new type of deck
 // which is a slice of strings
@@ -25,4 +29,23 @@ func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
 	}
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(fileName string) error {
+	// get current working directory
+	wd, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+	// create new directory
+	err = os.MkdirAll(wd+"\\output\\", 0666)
+	if err != nil {
+		return err
+	}
+	// save to new directory
+	return os.WriteFile(wd+"\\output\\"+fileName, []byte(d.toString()), 0666)
 }
