45e96350418d5087e362cc9d154afb73447f0f3b
ameyz7 <amey9765@hotmail.com>
2025-03-17 19:40:50 +0530

    even-odd | even or odd checker for slice

--- cards/main.go
+++ cards/main.go
@@ -3,10 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	card := newCard()
-	fmt.Println(card)
+
+	numbers := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	evenOrOdd(numbers)
 }
 
-func newCard() string {
-	return "Five of diamonds"
+func evenOrOdd(numbers []int) {
+	for _, num := range numbers {
+		if num%2 == 0 {
+			fmt.Printf("%v is even\n", num)
+		} else {
+			fmt.Printf("%v is odd\n", num)
+		}
+	}
 }
